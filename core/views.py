9b0ccfcf2c6709b11b68ab47ccbafdb6325420b5
kanweitech <kanweiedward@gmail.com>
2023-05-24 01:18:44 -0700

    generic class-based views commit

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from core.models import Snippet
+from core.serializers import SnippetSerializer
+from rest_framework import generics
 
-# Create your views here.
+# Using generic class-based views here.
+
+class SnippetList(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
