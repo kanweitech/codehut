c1af64582b8d490716e7278b637c729d0723b771
kanweitech <kanweiedward@gmail.com>
2023-05-24 01:31:50 -0700

    url.py commit

--- core/views.py
+++ core/views.py
@@ -4,6 +4,7 @@ from core.serializers import SnippetSerializer
 from rest_framework import generics
 
 # Using generic class-based views here.
+# This is a powerful pattern that allows us to reuse common functionality, and helps us keep our code DRY.
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
