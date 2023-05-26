ad5c09b8b0d8f388b09de98970d5972972e993fb
kanweitech <kanweiedward@gmail.com>
2023-05-25 19:26:58 -0700

    refactoring modules

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
 from core.models import Snippet
-from core.serializers import SnippetSerializer
+from core.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics
+from rest_framework import permissions
+
 
 # Using generic class-based views here.
 # This is a powerful pattern that allows us to reuse common functionality, and helps us keep our code DRY.
@@ -9,7 +12,20 @@ from rest_framework import generics
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
 class SnippetDetail(generics.RetrieveDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+def perform_create(self, serializer):
+    serializer.save(owner=self.request.user)
